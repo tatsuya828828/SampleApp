5a2dea39c9182928e04091888ca9f43119f36bb6
tatsuya828828 <kkjy011894@gmail.com>
2020-08-18 19:19:18 +0900

    UserRepositoryインタフェース作成

---
+++ src/main/java/com/kuma/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.kuma.repository;
+
+import java.util.List;
+
+import org.springframework.dao.DataAccessException;
+
+import com.kuma.model.UserModel;
+
+public interface UserRepository {
+	public int insert(UserModel user) throws DataAccessException;
+	public UserModel selectOne(String userId) throws DataAccessException;
+	public List<UserModel> selectMany() throws DataAccessException;
+	public int updateOne(UserModel user) throws DataAccessException;
+	public int deleteOne(String userId) throws DataAccessException;
+}
