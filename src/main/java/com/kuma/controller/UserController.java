e85dc0d34d4e842566b4e7b400cf44603ebb8685
tatsuya828828 <kkjy011894@gmail.com>
2020-08-21 11:45:14 +0900

    ユーザー一覧ページのGETメソッド用処理を追加

--- src/main/java/com/kuma/controller/UserController.java
+++ src/main/java/com/kuma/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.kuma.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -64,6 +66,8 @@ public class UserController {
 	@GetMapping("/userList")
 	public String getUserList(Model model) {
 		model.addAttribute("contents", "user/userList :: userList_contents");
+		List<UserModel> userList = userService.selectMany();
+		model.addAttribute("userList", userList);
 		return "/header";
 	}
 }
