09dfc19d338dc582e819f2c657ea8da42db05b20
tatsuya828828 <kkjy011894@gmail.com>
2020-08-24 20:03:06 +0900

    BookController作成

---
+++ src/main/java/com/kuma/controller/BookController.java
@@ -0,0 +1,56 @@
+package com.kuma.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.kuma.model.BookForm;
+import com.kuma.model.BookModel;
+import com.kuma.model.UserModel;
+import com.kuma.service.BookService;
+import com.kuma.service.UserService;
+
+@Controller
+public class BookController {
+	@Autowired
+	private BookService bookService;
+	@Autowired
+	private UserService userService;
+
+	@GetMapping("/bookNew")
+	public String getBookNew(@ModelAttribute BookForm bookForm, Model model) {
+		model.addAttribute("contents", "book/new :: bookNew_contents");
+		return "/header";
+	}
+
+	@PostMapping("/bookNew")
+	public String postBookNew(@ModelAttribute BookForm bookForm
+			, BindingResult bindingResult, Model model, HttpServletRequest httpServletRequest) {
+		if(bindingResult.hasErrors()) {
+			return getBookNew(bookForm, model);
+		}
+		System.out.println(bookForm);
+		String userId = httpServletRequest.getRemoteUser();
+		UserModel user = userService.selectOne(userId);
+		System.out.println("ログイン中のユーザー情報:"+ user);
+		BookModel book = new BookModel();
+		book.setTitle(bookForm.getTitle());
+		book.setBody(bookForm.getBody());
+		book.setUserId((String)user.getUserId());
+		System.out.println(user.getUserId());
+		System.out.println(book);
+		boolean result = bookService.insert(book);
+		if(result == true) {
+			System.out.println("登録成功");
+		} else {
+			System.out.println("登録失敗");
+		}
+		return "redirect:/mypage";
+	}
+}
