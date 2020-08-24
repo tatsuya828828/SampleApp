dbccc4b749b46b7531db9608f8a9d2a4a013489e
tatsuya828828 <kkjy011894@gmail.com>
2020-08-24 11:26:02 +0900

    デフォルトデータの追加

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,5 @@
 INSERT INTO user_table(user_id, password, name)
 VALUES('satou@gmail.com', '$2a$10$iMXSP71uLPvUgBEKaI3Kp.peKaVefaSad0os8ZV4vAsV9cVr9443O', 'さとう');
+
+INSERT INTO book_table(title, body, user_id)
+VALUES('アヒルと鴨のコインロッカー', '面白い', 'satou@gmail.com')
