3da8ae4fe43852dff3cb1b4d2304c6c77cfa4d7b
tatsuya828828 <kkjy011894@gmail.com>
2020-08-25 11:11:13 +0900

    hasBookメソッド追加

--- src/main/java/com/kuma/repository/UserRepository.java
+++ src/main/java/com/kuma/repository/UserRepository.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import org.springframework.dao.DataAccessException;
 
+import com.kuma.model.BookModel;
 import com.kuma.model.UserModel;
 
 public interface UserRepository {
 	public int insert(UserModel user) throws DataAccessException;
 	public UserModel selectOne(String userId) throws DataAccessException;
+	public List<BookModel> hasBook(String userId) throws DataAccessException;
 	public List<UserModel> selectMany() throws DataAccessException;
 	public int updateOne(UserModel user) throws DataAccessException;
 	public int deleteOne(String userId) throws DataAccessException;
