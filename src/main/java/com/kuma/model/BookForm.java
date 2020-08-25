49555910a3e2d66f7adafe4ceb65ca4a0475fb0e
tatsuya828828 <kkjy011894@gmail.com>
2020-08-25 19:33:55 +0900

    バリデーション追加

--- src/main/java/com/kuma/model/BookForm.java
+++ src/main/java/com/kuma/model/BookForm.java
@@ -1,10 +1,14 @@
 package com.kuma.model;
 
+import org.hibernate.validator.constraints.NotBlank;
+
 import lombok.Data;
 
 @Data
 public class BookForm {
+	@NotBlank(groups= Valid1.class)
 	public String title;
+	@NotBlank(groups= Valid1.class)
 	public String body;
 	public String userId;
 }
