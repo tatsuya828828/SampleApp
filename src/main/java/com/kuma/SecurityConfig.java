6dcbecf10724053d51340e8e5767c307704e5ce2
tatsuya828828 <kkjy011894@gmail.com>
2020-08-25 13:17:27 +0900

    ログイン後の遷移先を変更

--- src/main/java/com/kuma/SecurityConfig.java
+++ src/main/java/com/kuma/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 						.failureUrl("/login") // ログイン失敗時の遷移先
 						.usernameParameter("userId") // ユーザー名欄の値指定
 						.passwordParameter("password") // パスワード欄の値指定
-						.defaultSuccessUrl("/home", true); // ログイン成功後の遷移先
+						.defaultSuccessUrl("/bookList", true); // ログイン成功後の遷移先
 
 		// ログアウト処理
 		http.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
