a50afe61ee3c76a3b396f5e6aad08a7d06788436
tatsuya828828 <kkjy011894@gmail.com>
2020-08-26 14:28:16 +0900

    初期データ追加

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,8 @@
 INSERT INTO user_table(user_id, password, name)
 VALUES('satou@gmail.com', '$2a$10$iMXSP71uLPvUgBEKaI3Kp.peKaVefaSad0os8ZV4vAsV9cVr9443O', 'さとう');
 
+INSERT INTO user_table(user_id, password, name)
+VALUES('a@a', '$2a$10$dAoAeR1J9bSBbqM5RzQOme1J9DstuGlqWbgPxG7km8FH6pknl0zQy', 'a');
+
 INSERT INTO book_table(title, body, user_id)
 VALUES('アヒルと鴨のコインロッカー', '面白い', 'satou@gmail.com')
