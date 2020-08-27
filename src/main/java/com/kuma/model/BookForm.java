afb0c36ebb312e7593c1e6e58cc7d1ed18f17fe2
tatsuya828828 <kkjy011894@gmail.com>
2020-08-27 14:00:41 +0900

    タイトル変更用フィールド追加

--- src/main/java/com/kuma/model/BookForm.java
+++ src/main/java/com/kuma/model/BookForm.java
@@ -8,6 +8,7 @@ import lombok.Data;
 public class BookForm {
 	@NotBlank(groups= Valid1.class)
 	public String title;
+	public String newTitle;
 	@NotBlank(groups= Valid1.class)
 	public String body;
 	public String userId;
