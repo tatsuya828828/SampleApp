51b1054370dbacae153b90cb4128fdf6a9f8495b
tatsuya828828 <kkjy011894@gmail.com>
2020-08-31 18:50:51 +0900

    カラム修正

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,8 @@
-CREATE TABLE IF NOT EXISTS user_table(
-	user_id VARCHAR(50) PRIMARY KEY, password VARCHAR(100), name VARCHAR(50));
+CREATE TABLE IF NOT EXISTS user(
+	id VARCHAR(50) PRIMARY KEY, password VARCHAR(100), name VARCHAR(50));
 
-CREATE TABLE IF NOT EXISTS book_table(
-	title VARCHAR(50) PRIMARY KEY, new_title VARCHAR(50), body VARCHAR(100), user_id VARCHAR(200));
+CREATE TABLE IF NOT EXISTS book(
+	title VARCHAR(50) PRIMARY KEY, new_title VARCHAR(50), body VARCHAR(100), user_id VARCHAR(50) REFERENCES user(id));
+CREATE TABLE IF NOT EXISTS comment_table(
+	user_id VARCHAR(50) REFERENCES user(id), book_id VARCHAR(50) REFERENCES book(title)
+	, comment VARCHAR(50));
