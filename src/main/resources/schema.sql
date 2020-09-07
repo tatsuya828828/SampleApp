3d6896c9b2602653835da630f4b40aa7af9b72dd
tatsuya828828 <kkjy011894@gmail.com>
2020-09-07 19:52:17 +0900

    evaluationテーブル追加,bookテーブルにevaluationカラムを追加

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,7 +2,10 @@ CREATE TABLE IF NOT EXISTS user(
 	id VARCHAR(50) PRIMARY KEY, password VARCHAR(100), name VARCHAR(50));
 
 CREATE TABLE IF NOT EXISTS book(
-	title VARCHAR(50) PRIMARY KEY, new_title VARCHAR(50), body VARCHAR(100), author VARCHAR(50), user_id VARCHAR(50) REFERENCES user(id));
+	title VARCHAR(50) PRIMARY KEY, new_title VARCHAR(50),
+	body VARCHAR(100), author VARCHAR(50), user_id VARCHAR(50) REFERENCES user(id), evaluation INT);
 CREATE TABLE IF NOT EXISTS comment(
 	user_id VARCHAR(50) REFERENCES user(id), book_id VARCHAR(50) REFERENCES book(title)
 	, comment VARCHAR(50));
+CREATE TABLE IF NOT EXISTS evaluation(
+	evaluation INT, user_id VARCHAR(50) REFERENCES user(id), book_id VARCHAR(50) REFERENCES book(title))
