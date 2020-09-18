e18d5e8255b8684dc7276ac07a2d2d1f28b5252a
tatsuya828828 <kkjy011894@gmail.com>
2020-09-18 19:06:19 +0900

    データを追加

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,7 +4,7 @@ VALUES('satou@gmail.com', '$2a$10$iMXSP71uLPvUgBEKaI3Kp.peKaVefaSad0os8ZV4vAsV9c
 INSERT INTO user(self_id, password, name)
 VALUES('a@a', '$2a$10$dAoAeR1J9bSBbqM5RzQOme1J9DstuGlqWbgPxG7km8FH6pknl0zQy', 'a');
 -- bookのデータ
-INSERT INTO book VALUES(NULL, 'アヒルと鴨のコインロッカー', '面白い', '伊坂幸太郎', 1, 0);
-INSERT INTO book VALUES(NULL, 'スッキリわかるJava入門', 'わかりやすい', '中山清喬', 2, 0);
-INSERT INTO book VALUES(NULL, 'スッキリわかるJava入門 実践編', 'わかりやすい', '中山清喬', 2, 0);
-INSERT INTO book VALUES(NULL, 'スッキリわかるSQL入門', 'わかりやすい', '中山清喬', 2, 0);
+INSERT INTO book VALUES(NULL, 'アヒルと鴨のコインロッカー', '面白い', '伊坂幸太郎', '小説', 1, 0);
+INSERT INTO book VALUES(NULL, 'スッキリわかるJava入門', 'わかりやすい', '中山清喬', '学習本', 2, 0);
+INSERT INTO book VALUES(NULL, 'スッキリわかるJava入門 実践編', 'わかりやすい', '中山清喬', '学習本', 2, 0);
+INSERT INTO book VALUES(NULL, 'スッキリわかるSQL入門', 'わかりやすい', '中山清喬', '学習本', 2, 0);
