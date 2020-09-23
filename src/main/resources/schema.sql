fa99f02e7df42db9b2e07df8704341b4a6e45fb5
tatsuya828828 <kkjy011894@gmail.com>
2020-09-23 11:47:23 +0900

    投稿日カラム追加

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS book(
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, CREATED_AT DATE, title VARCHAR(50),
 	body VARCHAR(100), author VARCHAR(50), genre VARCHAR(50), user_id INT REFERENCES user(id), evaluation INT);
 CREATE TABLE IF NOT EXISTS comment(
-	user_id INT REFERENCES user(id), book_id INT REFERENCES book(id)
+	CREATED_AT DATE, user_id INT REFERENCES user(id), book_id INT REFERENCES book(id)
 	, comment VARCHAR(50));
 CREATE TABLE IF NOT EXISTS evaluation(
 	evaluation INT, user_id INT REFERENCES user(id), book_id INT REFERENCES book(id), PRIMARY KEY(user_id, book_id))
