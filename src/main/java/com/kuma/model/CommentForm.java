846f360041c6cefdd4e5cdc0efbb9853030693d5
tatsuya828828 <kkjy011894@gmail.com>
2020-09-23 11:50:44 +0900

    field追加

--- src/main/java/com/kuma/model/CommentForm.java
+++ src/main/java/com/kuma/model/CommentForm.java
@@ -1,9 +1,12 @@
 package com.kuma.model;
 
+import java.util.Date;
+
 import lombok.Data;
 
 @Data
 public class CommentForm {
+	public Date createdAt;
 	public UserModel user;
 	public BookModel book;
 	public String comment;
--- src/main/java/com/kuma/model/CommentModel.java
+++ src/main/java/com/kuma/model/CommentModel.java
@@ -1,9 +1,12 @@
 package com.kuma.model;
 
+import java.util.Date;
+
 import lombok.Data;
 
 @Data
 public class CommentModel {
+	private Date createdAt;
 	private String comment;
 	private UserModel user;
 	private BookModel book;
