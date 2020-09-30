8df7034979b9258fc757ef653326f1377a9c25e1
tatsuya828828 <kkjy011894@gmail.com>
2020-09-30 23:28:59 +0900

    userテーブルにimageカラム追加

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
-INSERT INTO user(created_at, last_login, self_id, password, name)
-VALUES(CURRENT_DATE, LOCALTIME, 'satou@gmail.com', '$2a$10$iMXSP71uLPvUgBEKaI3Kp.peKaVefaSad0os8ZV4vAsV9cVr9443O', 'さとう');
+INSERT INTO user(created_at, last_login, self_id, password, name, image)
+VALUES(CURRENT_DATE, LOCALTIME, 'satou@gmail.com', '$2a$10$iMXSP71uLPvUgBEKaI3Kp.peKaVefaSad0os8ZV4vAsV9cVr9443O', 'さとう', '/images/NOIMAGE.png');
 
-INSERT INTO user(created_at, last_login, self_id, password, name)
-VALUES(CURRENT_DATE, LOCALTIME, 'a@a', '$2a$10$dAoAeR1J9bSBbqM5RzQOme1J9DstuGlqWbgPxG7km8FH6pknl0zQy', 'a');
+INSERT INTO user(created_at, last_login, self_id, password, name, image)
+VALUES(CURRENT_DATE, LOCALTIME, 'a@a', '$2a$10$dAoAeR1J9bSBbqM5RzQOme1J9DstuGlqWbgPxG7km8FH6pknl0zQy', 'a', '/images/NOIMAGE.png');
 -- bookのデータ
 INSERT INTO book VALUES(NULL, CURRENT_DATE, 'アヒルと鴨のコインロッカー', '面白い', '伊坂幸太郎', '小説', 1, 0, '/images/1_ahiru.jpg');
 INSERT INTO book VALUES(NULL, CURRENT_DATE, 'スッキリわかるJava入門', 'わかりやすい', '中山清喬', '学習本', 2, 0, '/images/NOIMAGE.png');
