c79a9daae6043dd2e7fbb7bde83e68cb8b532051
tatsuya828828 <kkjy011894@gmail.com>
2020-10-01 11:45:30 +0900

    insert,updateの引数に値を追加

--- src/main/java/com/kuma/repository/UserRepository.java
+++ src/main/java/com/kuma/repository/UserRepository.java
@@ -3,17 +3,18 @@ package com.kuma.repository;
 import java.util.List;
 
 import org.springframework.dao.DataAccessException;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.kuma.model.BookModel;
 import com.kuma.model.UserModel;
 
 public interface UserRepository {
-	public int insert(UserModel user) throws DataAccessException;
+	public int insert(UserModel user, MultipartFile multipartFile) throws DataAccessException;
 	public UserModel selectOne(int id) throws DataAccessException;
 	public UserModel currentUser(String selfId) throws DataAccessException;
 	public List<BookModel> hasBook(int id) throws DataAccessException;
 	public List<UserModel> selectMany() throws DataAccessException;
-	public int updateOne(UserModel user) throws DataAccessException;
+	public int updateOne(UserModel user, MultipartFile multipartFile) throws DataAccessException;
 	public int deleteOne(int id) throws DataAccessException;
 	public int saveLoginTime(String selfId) throws DataAccessException;
 }
