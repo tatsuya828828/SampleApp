03e8e9ddb7361e240fe9e839b9a6f682dbb83dd6
tatsuya828828 <kkjy011894@gmail.com>
2020-10-02 13:55:12 +0900

    評価の登録・更新処理追加

--- src/main/java/com/kuma/repository/CommentRepository.java
+++ src/main/java/com/kuma/repository/CommentRepository.java
@@ -7,7 +7,7 @@ import org.springframework.dao.DataAccessException;
 import com.kuma.model.CommentModel;
 
 public interface CommentRepository {
-	public int insert(CommentModel comment) throws DataAccessException;
+	public int selectComment(CommentModel comment) throws DataAccessException;
 	public List<CommentModel> selectMany(int bookId) throws DataAccessException;
 	public int delete(int userId, int bookId) throws DataAccessException;
 }
