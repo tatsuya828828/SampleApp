561cead2b0caf8f6d63586e674951c4ce5223028
tatsuya828828 <kkjy011894@gmail.com>
2020-10-02 14:07:18 +0900

    評価登録・更新用処理追加

--- src/main/java/com/kuma/controller/CommentController.java
+++ src/main/java/com/kuma/controller/CommentController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.kuma.model.BookModel;
 import com.kuma.model.CommentForm;
@@ -28,18 +29,34 @@ public class CommentController {
 
 	@PostMapping("/bookDetail/{bookId}/postComment")
 	public String postComment(@ModelAttribute CommentForm form,
-			Model model, HttpServletRequest httpServletRequest, @PathVariable("bookId") int bookId) {
+			Model model, HttpServletRequest httpServletRequest
+			, @PathVariable("bookId") int bookId, @RequestParam("num") int num) {
+		System.out.println("数値:"+ num);
 		UserModel user = userService.currentUser(httpServletRequest.getRemoteUser());
 		BookModel book = bookService.selectOne(bookId);
 		CommentModel comment = new CommentModel();
+		int id = 0;
+		String word = "登録";
+		if(form.getId()>0) {
+			id = form.getId();
+			word = "更新";
+		}
+		comment.setId(id);
 		comment.setUser(user);
 		comment.setBook(book);
 		comment.setComment((String) form.getComment());
-		boolean result = commentService.insert(comment);
+		comment.setEvaluation(num);
+		boolean result = commentService.selectComment(comment);
 		if(result == true) {
-			System.out.println("登録成功");
+			System.out.println(word +"成功");
 		} else {
-			System.out.println("登録失敗");
+			System.out.println(word +"処理失敗");
+		}
+		boolean result2 = bookService.updateEvaluation(comment.getBook().getId());
+		if(result2 == true) {
+			System.out.println("平均評価更新成功");
+		} else {
+			System.out.println("平均評価更新失敗");
 		}
 		return "redirect:/bookDetail/{bookId}";
 	}
