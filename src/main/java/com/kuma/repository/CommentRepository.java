5300200a1b3910cf974c27ce166429210ab3bb5b
tatsuya828828 <kkjy011894@gmail.com>
2020-10-05 20:38:22 +0900

    メソッド整理、1件取得用メソッド追加

--- src/main/java/com/kuma/repository/CommentRepository.java
+++ src/main/java/com/kuma/repository/CommentRepository.java
@@ -7,8 +7,10 @@ import org.springframework.dao.DataAccessException;
 import com.kuma.model.CommentModel;
 
 public interface CommentRepository {
-	public int selectAction(CommentModel comment) throws DataAccessException;
-	public boolean selectOne(int userId, int bookId) throws DataAccessException;
+	public int insert(CommentModel comment) throws DataAccessException;
+	public CommentModel selectOne(int commentId) throws DataAccessException;
+	public int update(CommentModel comment) throws DataAccessException;
+	public boolean confirmComment(int userId, int bookId) throws DataAccessException;
 	public List<CommentModel> selectMany(int bookId) throws DataAccessException;
 	public int delete(int userId, int bookId) throws DataAccessException;
 }
