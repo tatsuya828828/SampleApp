820ce56e073c07a7d70e6f1605a5c9d64310a7ca
tatsuya828828 <kkjy011894@gmail.com>
2020-10-06 11:13:21 +0900

    replyテーブル作成

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,6 +6,11 @@ CREATE TABLE IF NOT EXISTS book(
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, created_at DATE, title VARCHAR(50),
 	body VARCHAR(100), author VARCHAR(50), genre VARCHAR(50)
 	, user_id INT REFERENCES user(id), evaluation INT, image VARCHAR(50));
+
 CREATE TABLE IF NOT EXISTS comment(
 	id INT NOT NULL AUTO_INCREMENT, created_at DATE, user_id INT REFERENCES user(id), book_id INT REFERENCES book(id)
 	, comment VARCHAR(500), evaluation INT, PRIMARY KEY(user_id, book_id));
+
+CREATE TABLE IF NOT EXISTS reply(
+	id INT NOT NULL AUTO_INCREMENT, created_at DATE, user_id INT REFERENCES user(id), comment_id INT REFERENCES comment(id)
+	, reply VARCHAR(500), PRIMARY KEY(user_id, comment_id, reply_id));
