90b47c42cf2c18c9f18ce481835c34914cdf8c8d
tatsuya828828 <kkjy011894@gmail.com>
2020-10-12 11:25:45 +0900

    リプライ更新処理追加

--- src/main/java/com/kuma/controller/ReplyController.java
+++ src/main/java/com/kuma/controller/ReplyController.java
@@ -33,6 +33,15 @@ public class ReplyController {
 	@Autowired
 	UserService userService;
 
+	// 成否を知らせるメソッド
+	public void result(boolean result, String word) {
+		if(result == true) {
+			System.out.println(word+"成功");
+		} else {
+			System.out.println(word+"失敗");
+		}
+	}
+
 	@GetMapping("/bookDetail/{bookId}/postReply/{commentId}")
 	public String getPostReply(@ModelAttribute ReplyForm form, Model model
 			, @PathVariable("bookId") int bookId, @PathVariable("commentId") int commentId
@@ -68,11 +77,50 @@ public class ReplyController {
 		reply.setUser(user);
 		reply.setComment(comment);
 		boolean result = replyService.insert(reply);
-		if(result == true) {
-			System.out.println("返信成功");
-		} else {
-			System.out.println("返信失敗");
+		result(result, "返信");
+		return "redirect:/bookDetail/{bookId}";
+	}
+
+	@GetMapping("/bookDetail/{bookId}/editReply/{replyId}")
+	public String getEditReply(@ModelAttribute ReplyForm form, Model model
+			, HttpServletRequest httpServletRequest, @PathVariable("replyId") int replyId
+			, @PathVariable("bookId") int bookId) {
+		if(String.valueOf(replyId).length()>0) {
+			ReplyModel reply = replyService.selectOne(replyId);
+			CommentModel comment = commentService.selectOne(reply.getComment().getId());
+			BookModel book = bookService.selectOne(bookId);
+			UserModel user = userService.currentUser(httpServletRequest.getRemoteUser());
+			if(reply.getUser().getId() == user.getId()) {
+				form.setComment(comment);
+				form.setUser(reply.getUser());
+				form.setReply(reply.getReply());
+				model.addAttribute("contents", "book/bookDetail :: bookDetail_contents");
+				model.addAttribute("commentList", "book/commentList :: comment_list");
+				model.addAttribute("editReply", "book/editReply :: edit_reply");
+				model.addAttribute("replyList", "book/replyList :: reply_list");
+				model.addAttribute("editReplyId", replyId);
+			} else {
+				return "redirect:/bookDetail/{id}";
+			}
+			model.addAttribute("book", book);
+			List<CommentModel> comments = commentService.selectMany(bookId);
+			model.addAttribute("comments", comments);
+			int count = bookService.evaluationCount(bookId);
+			model.addAttribute("count", count);
+			boolean result = commentService.confirmComment(user.getId(), book.getId());
+			model.addAttribute("result", result);
 		}
+		return "/header";
+	}
+
+	@PostMapping("/bookDetail/{bookId}/editReply/{replyId}")
+	public String postEditComment(@ModelAttribute ReplyForm form, Model model
+			, @PathVariable("replyId") int replyId, @PathVariable("bookId") int bookId) {
+		ReplyModel reply = new ReplyModel();
+		reply.setId(replyId);
+		reply.setReply(form.getReply());
+		boolean result = replyService.update(reply);
+		result(result, "更新");
 		return "redirect:/bookDetail/{bookId}";
 	}
 }
