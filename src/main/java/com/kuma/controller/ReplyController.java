dcb5239c27984338a3f356fea590688b5cd95b31
tatsuya828828 <kkjy011894@gmail.com>
2020-10-13 15:44:26 +0900

    投稿、編集の際のリダイレクト時に値を渡す処理を追加

--- src/main/java/com/kuma/controller/ReplyController.java
+++ src/main/java/com/kuma/controller/ReplyController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.kuma.model.BookModel;
 import com.kuma.model.CommentModel;
@@ -69,7 +70,8 @@ public class ReplyController {
 
 	@PostMapping("/bookDetail/{bookId}/postReply/{commentId}")
 	public String postReply(@ModelAttribute ReplyForm form, HttpServletRequest httpServletRequest
-			, @PathVariable("commentId") int commentId, @PathVariable("bookId") int bookId) {
+			, @PathVariable("commentId") int commentId, @PathVariable("bookId") int bookId
+			, RedirectAttributes redirectAttributes) {
 		UserModel user = userService.currentUser(httpServletRequest.getRemoteUser());
 		CommentModel comment = commentService.selectOne(commentId);
 		ReplyModel reply = new ReplyModel();
@@ -78,7 +80,8 @@ public class ReplyController {
 		reply.setComment(comment);
 		boolean result = replyService.insert(reply);
 		result(result, "返信");
-		return "redirect:/bookDetail/{bookId}";
+		redirectAttributes.addFlashAttribute("commentId", commentId);
+		return "redirect:/bookDetail/{bookId}#bottom{commentId}";
 	}
 
 	@GetMapping("/bookDetail/{bookId}/editReply/{replyId}")
@@ -98,7 +101,9 @@ public class ReplyController {
 				model.addAttribute("commentList", "book/commentList :: comment_list");
 				model.addAttribute("editReply", "book/editReply :: edit_reply");
 				model.addAttribute("replyList", "book/replyList :: reply_list");
+				model.addAttribute("test", "open");
 				model.addAttribute("editReplyId", replyId);
+				model.addAttribute("commentId", reply.getComment().getId());
 			} else {
 				return "redirect:/bookDetail/{id}";
 			}
@@ -115,13 +120,16 @@ public class ReplyController {
 
 	@PostMapping("/bookDetail/{bookId}/editReply/{replyId}")
 	public String postEditComment(@ModelAttribute ReplyForm form, Model model
-			, @PathVariable("replyId") int replyId, @PathVariable("bookId") int bookId) {
+			, @PathVariable("replyId") int replyId, @PathVariable("bookId") int bookId
+			, RedirectAttributes redirectAttributes) {
+		int commentId = replyService.selectOne(replyId).getComment().getId();
 		ReplyModel reply = new ReplyModel();
 		reply.setId(replyId);
 		reply.setReply(form.getReply());
 		boolean result = replyService.update(reply);
 		result(result, "更新");
-		return "redirect:/bookDetail/{bookId}";
+		redirectAttributes.addFlashAttribute("commentId", commentId);
+		return "redirect:/bookDetail/{bookId}#reply{replyId}";
 	}
 
 	@PostMapping(value="/bookDetail/{bookId}/editReply/{replyId}", params="delete")
